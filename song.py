0f065920e4b2b0134b3c94c2135386eb2e06a7ba
Misaka2623 <415470991@qq.com>
2018-03-01 18:16:53 -0700

    add comments

--- song.py
+++ song.py
@@ -1,3 +1,10 @@
+"""
+spec: http://allisonobourn.com/110/spring18/homework/1/spec.pdf
+
+This program is to print a song to the console.
+"""
+
+
 PERIOD = ". "
 COMMA = ", "
 FLY = "fly"
@@ -9,6 +16,8 @@ HORSE = "horse"
 
 
 def main():
+    """ The main function of this program.
+    """
     paragraph1()
     paragraph2()
     paragraph3()
@@ -18,11 +27,15 @@ def main():
 
 
 def paragraph1():
+    """ Prints the first paragraph of the song.
+    """
     action(FLY + PERIOD)
     question()
 
 
 def paragraph2():
+    """ Prints the second paragraph of the song.
+    """
     action(SPIDER + COMMA)
     print("That wriggled and iggled and jiggled inside her. ")
     purpose(SPIDER, FLY)
@@ -30,6 +43,8 @@ def paragraph2():
 
 
 def paragraph3():
+    """ Prints the third paragraph of the song.
+    """
     action(BIRD + COMMA)
     print("How absurd to swallow a bird. ")
     purpose(BIRD, SPIDER)
@@ -38,6 +53,8 @@ def paragraph3():
 
 
 def paragraph4():
+    """ Prints the fourth paragraph of the song.
+    """
     action(CAT + COMMA)
     print("Imagine that to swallow a cat. ")
     purpose(CAT, BIRD)
@@ -47,6 +64,8 @@ def paragraph4():
 
 
 def paragraph5():
+    """ Prints the fifth paragraph of the song.
+    """
     action(DOG + COMMA)
     print("What a hog to swallow a dog. ")
     purpose(DOG, CAT)
@@ -57,21 +76,26 @@ def paragraph5():
 
 
 def paragraph6():
+    """ Prints the sixth paragraph of the song.
+    """
     action(HORSE)
     print("She died of course. ")
 
 
 def action(food):
+    """ Prints the action of the old woman.
+
+    :param food: the animal that the old woman ate.
+    """
     assert isinstance(food, str)
     print("There was an old woman who swallowed a", food)
 
 
 def purpose(predator, food):
-    """ the purpose why the old woman swallowed the predator
+    """ Prints the purpose why the old woman swallowed the predator
 
     :param predator: the name of the predator
     :param food: the name of the food
-    :return:
     """
     assert isinstance(predator, str)
     assert isinstance(food, str)
@@ -79,6 +103,8 @@ def purpose(predator, food):
 
 
 def question():
+    """ Prints the last sentences of some paragraphs.
+    """
     print("I don't know why she swallowed that fly, ")
     print("Perhaps she'll dies. ")
     print()
